64848489babccfb0f33b6022e7d64c60c05b18ac
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-06 19:04:57 +0530

    doing

--- client/src/pages/RegisterNow.jsx
+++ client/src/pages/RegisterNow.jsx
@@ -41,8 +41,8 @@ const Register = ({setIsLogin}) => {
   return (
     <div className="w-full flex flex-col gap-8 items-center justify-center  md:mt-36 mt-12">
       <div className="w-[90%] md:w-[50%] border-2 border-[rgba(109,40,217)] shadow-[5px_5px_0px_0px_rgba(109,40,217)] rounded-lg p-4 flex flex-col items-center justify-center gap-4">
-        <div className="group flex justify-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg overflow-hidden">
-          <div className="hidden md:flex">
+        <div className="group flex items-center justify-center flex-col border-2 border-dotted bg-white border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg overflow-hidden">
+          <div className="hidden md:flex w-1/2 mt-20">
             <Lottie animationData={register} />
           </div>
           <div className="flex md:hidden">
