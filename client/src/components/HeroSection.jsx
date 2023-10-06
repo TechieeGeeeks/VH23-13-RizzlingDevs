078a21caf6cbd04fa85063fe5cb3a229944155a1
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-06 15:10:35 +0530

    Hero

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Lottie from "lottie-react";
 import HeroAnimation from "../img/heroAnimation.json";
-import CustomButton from "./CustomButton";
 
 const HeroSection = () => {
   return (
-    <div className=" grid md:grid-cols-2 grid-cols-1 items-center ">
+    <div className=" grid md:grid-cols-2 grid-cols-1 items-center mt-12 md:mt-0 ">
       <div className=" flex flex-col gap-6 pb-14 md:text-left text-center">
         <p className=" md:text-[3rem] text-[2.5rem] font-medium leading-tight">
           THE SMARTEST WAY TO BUILD YOUR WEB3{" "}
@@ -15,7 +14,7 @@ const HeroSection = () => {
           We’re offering all projects, no matter their size, the opportunity to
           post jobs for free for 7 days. That’s not an offer. That’s forever.
         </p>
-        <div>
+        {/* <div>
           <a href="#">
             <CustomButton
               text={"Find Job"}
@@ -27,7 +26,7 @@ const HeroSection = () => {
           <a href="#">
             <CustomButton text={"Post Job"} hoverColor={"lightBlue"} />
           </a>
-        </div>
+        </div> */}
       </div>
       <div className="pb-14 ">
         <Lottie
