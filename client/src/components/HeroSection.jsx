f7f40e430193214b6d9dcd25eb6301dc7334dc9f
Abhishek yadav <barrelabhi45@gmail.com>
2023-10-06 15:56:32 +0530

    Hero Section Updated

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Lottie from "lottie-react";
-import HeroAnimation from "../img/heroAnimation.json";
+import hero from "../img/hero.json";
 
 const HeroSection = () => {
   return (
     <div className=" grid md:grid-cols-2 grid-cols-1 items-center mt-12 md:mt-0 ">
       <div className=" flex flex-col gap-6 pb-14 md:text-left text-center">
         <p className=" md:text-[3rem] text-[2.5rem] font-medium leading-tight">
-          THE SMARTEST WAY TO BUILD YOUR WEB3{" "}
-          <span className=" text-lightBlue ">CAREER</span>
+        GET YOUR SECURED AND VERIFIED{" "}
+          <span className=" text-purpleColor ">CERTIFICATES</span>
         </p>
         <p className=" text-gray-400">
-          We’re offering all projects, no matter their size, the opportunity to
-          post jobs for free for 7 days. That’s not an offer. That’s forever.
+        Certify Blocks provides a seamless and secure certificate management solution, ensuring that your credentials are easily accessible, tamper-proof, and instantly verifiable.
         </p>
         {/* <div>
           <a href="#">
@@ -30,7 +29,7 @@ const HeroSection = () => {
       </div>
       <div className="pb-14 ">
         <Lottie
-          animationData={HeroAnimation}
+          animationData={hero}
           className="border md:border-none bg-white md:bg-transparent rounded-lg"
         />
       </div>
