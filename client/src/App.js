626a47319e0581b91d214c5001426c34f8565127
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-06 16:12:51 +0530

    Certification genration

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,57 @@ import Login from "./pages/Login";
 import RegisterNow from "./pages/RegisterNow";
 import Courses from "./components/Courses";
 import MintCertificate from "./pages/MintCertificate.jsx";
+import Demo from "./components/Demo";
+import { useState, createContext } from "react";
+import axios from "axios";
+import InputForm from "./components/qr-generation/InputForm";
+import QrCode from "./components/qr-generation/QrCode";
 
+export const InputContext = createContext();
 
 function App() {
+  const [inputValue, setInputValue] = useState({
+    url: "",
+    color: "",
+  });
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const config = {
+    headers: { Authorization: "Bearer 2b38fc30-6420-11ee-a74e-41d810857b10" },
+  };
+  const bodyParameters = {
+    colorDark: inputValue.color,
+    qrCategory: "url",
+    text: inputValue.url,
+  };
+  const getQrCode = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post(
+        "https://qrtiger.com/api/qr/static",
+        bodyParameters,
+        config
+      );
+      setResponse(res.data.url);
+      console.log(response);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const value = {
+    inputValue,
+    setInputValue,
+    getQrCode,
+    response,
+    loading,
+    error,
+  };
+
   return (
     <div className="md:p-6 md:px-64 p-7">
       <Navbar />
@@ -19,14 +67,26 @@ function App() {
       <Routes>
         <Route path="/*" element={<HeroSection />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register-now" element={<RegisterNow />}/>
-        <Route path="/courses" element={<Courses />}/>
-        <Route path="/certificate/create" element={<MintCertificate />}/>
+        <Route path="/register-now" element={<RegisterNow />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/certificate/create" element={<MintCertificate />} />
       </Routes>
       {/* <Demo /> */}
       {/* <HeroSection />
        <CertificateWithExport className="Your Class Name" /> */}
-        <CertificateWithExport className="Your Class Name" /> 
+      {/* <CertificateWithExport className="Your Class Name" />  */}
+      <Demo
+        name="John Doe"
+        title="React Master"
+        date="October 6, 2023"
+        logo={response}
+        hash="12345"
+      />
+
+      <InputContext.Provider value={value}>
+        <InputForm />
+        <QrCode />
+      </InputContext.Provider>
     </div>
   );
 }
--- client/src/components/Demo.jsx
+++ client/src/components/Demo.jsx
@@ -1,111 +1,78 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Demo = ({ title, name, date, hash, logo }) => {
-  const qrCodeData = `ID: ${hash}`; // Data for the QR code
-  const qrCodeSize = 150; // Size of the QR code square
-
-  // Function to generate the QR code as SVG elements
-  const generateQRCodeSVG = () => {
-    const qrCodeElements = [];
-    const qrCode = qrCodeData.split("");
-
-    // Create the QR code squares
-    for (let row = 0; row < qrCodeSize; row++) {
-      for (let col = 0; col < qrCodeSize; col++) {
-        const x = col * 5; // Each square is 5 units wide
-        const y = row * 5; // Each square is 5 units tall
-        const fill = qrCode.shift() === "1" ? "#000" : "#fff"; // Fill color based on QR code data
-
-        qrCodeElements.push(
-          <rect key={`${row}-${col}`} x={x} y={y} width="5" height="5" fill={fill} />
-        );
-      }
-    }
-
-    return qrCodeElements;
-  };
-
+function Demo({ title, name, date, hash, logo }) {
+  
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="800" height="500" id="certificate">
-      {/* QR Code */}
-      <g transform="translate(50, 50)">
-        {generateQRCodeSVG()}
-      </g>
-
-      {/* Certificate Border */}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1000"
+      height="700"
+      id="certificate"
+    >
       <rect
         x="50"
         y="25"
         rx="20"
         ry="20"
-        width="700"
-        height="450"
+        width="900"
+        height="600"
         id="border"
-        fill="none"
-        stroke="#000000"
-        stroke-width="3"
+        fill="white"
+        stroke="black"
       />
-
-      {/* Certificate Title */}
-      <text x="400" y="100" text-anchor="middle" fill="#000" id="bodyTitle" font-size="30" font-weight="bold">
+      <text x="500" y="100" text-anchor="middle" fill="black" id="bodyTitle">
         Certificate
       </text>
-
-      {/* "of" Subtitle */}
-      <text x="400" y="140" text-anchor="middle" fill="#000" id="bodySubTitle" font-size="20">
+      <text x="500" y="125" text-anchor="middle" fill="black" id="bodySubTitle">
         of
       </text>
-
-      {/* Title Underline */}
-      <line x1="250" y1="170" x2="550" y2="170" id="titleUnderLine" stroke="#000" stroke-width="2" />
-
-      {/* Certificate Title Text */}
-      <text x="400" y="160" text-anchor="middle" fill="#000" id="title" font-size="28" font-weight="bold">
+      <line x1="250" y1="260" x2="750" y2="260" id="titleUnderLine" />
+      <text x="500" y="250" text-anchor="middle" fill="black" id="title">
         {title}
       </text>
-
-      {/* "Awarded to" Subtitle */}
-      <text x="400" y="220" text-anchor="middle" fill="#000" id="subTitleHeader" font-size="20">
+      <text
+        x="500"
+        y="300"
+        text-anchor="middle"
+        fill="black"
+        id="subTitleHeader"
+      >
         awarded to
       </text>
-
-      {/* Recipient Name */}
-      <text x="400" y="270" text-anchor="middle" fill="#000" id="name" font-size="26">
+      <text x="500" y="400" text-anchor="middle" fill="black" id="name">
         {name}
       </text>
-
-      {/* Name Underline */}
-      <line x1="250" y1="290" x2="550" y2="290" id="titleUnderLine" stroke="#000" stroke-width="2" />
-
-      {/* "on" Subtitle */}
-      <text x="400" y="320" text-anchor="middle" fill="#000" id="bodySubTitle" font-size="20">
+      <line x1="200" y1="410" x2="800" y2="410" id="titleUnderLine" />
+      <text x="500" y="440" text-anchor="middle" fill="black" id="bodySubTitle">
         on
       </text>
-
-      {/* Date */}
-      <text x="400" y="360" text-anchor="middle" fill="#000" id="date" font-size="24">
+      <text x="500" y="500" text-anchor="middle" fill="black" id="date">
         {date}
       </text>
-
-      {/* Date Underline */}
-      <line x1="250" y1="380" x2="550" y2="380" id="titleUnderLine" stroke="#000" stroke-width="2" />
-
-      {/* ID */}
-      <text x="70" y="480" text-anchor="start" fill="#000" id="hash" font-size="18">
+      <line x1="400" y1="510" x2="600" y2="510" id="titleUnderLine" />
+      <text x="100" y="575" text-anchor="start" fill="black" id="hash">
         ID: {hash}
       </text>
-
-      {/* Logo */}
       <image
-        x="625"
-        y="410"
-        height="80px"
-        width="120px"
+        x="650"
+        y="300"
+        height="200px"
+        width="200px"
         id="logo"
         href={logo}
       />
+      Sorry, your browser does not support inline SVG.
     </svg>
   );
-};
+}
+
+Demo.propTypes = {
+    name: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    logo: PropTypes.string.isRequired,
+    hash: PropTypes.string.isRequired,
+}
 
 export default Demo;
