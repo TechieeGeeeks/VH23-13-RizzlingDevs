7917cba5c5bc87a1f6a3b895416f476c608d2acc
vwakesahu <89719144+vwakesahu@users.noreply.github.com>
2023-10-06 15:11:47 +0530

    Hero

--- client/src/pages/RegisterNow.jsx
+++ client/src/pages/RegisterNow.jsx
@@ -3,7 +3,6 @@ import Lottie from "lottie-react";
 import register from "../img/register.json";
 import login from "../img/login.json";
 
-
 const Login = () => {
   const [imageAsset, setimageAsset] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -11,7 +10,7 @@ const Login = () => {
   const [email, setemail] = useState("");
   const [password, setpassword] = useState(null);
   return (
-    <div className="w-full flex flex-col gap-8 items-center justify-center mt-36">
+    <div className="w-full flex flex-col gap-8 items-center justify-center  md:mt-36 mt-12">
       <div className="w-[90%] md:w-[50%] border-2 border-[rgba(109,40,217)] shadow-[5px_5px_0px_0px_rgba(109,40,217)] rounded-lg p-4 flex flex-col items-center justify-center gap-4">
         <div className="group flex justify-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-340 cursor-pointer rounded-lg overflow-hidden">
           <div className="hidden md:flex">
